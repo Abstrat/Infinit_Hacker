20e887de5a6e24a2fc10653b46a8bb7e57bf8164
Daniel Wemmer <daniel.wemmer.dw@gmail.com>
2016-03-04 17:02:07 +0100

    Added Project
    Added Project + Some Scripts + Some basic Textures

---
+++ Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Movement : MonoBehaviour {
+
+	Rigidbody2D rbody;
+	// Animator anim;
+
+	// Use this for initialization
+	void Start () {
+
+		//anim = GetComponent<Animator> ();
+		rbody = GetComponent<Rigidbody2D> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Vector2 movment_vector = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+
+		/**
+		 * 	if (movement_vector != Vector2.zero)
+		 * 	{
+		 * 		anim.SetBool("isWalking", true);
+		 * 		anim.SetFloat("input_x", movement_vector.x);
+		 * 		anim.SetFloat("input_y", movement_vector.y);
+		 * 	} else {
+		 * 		anim.SetBool("isWalking", false);
+		 * 	}
+		 **/
+
+		rbody.MovePosition (rbody.position + movment_vector * Time.deltaTime);
+
+	}
+}
---
+++ Assets/Scripts/Smooth_Follow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Smooth_Follow : MonoBehaviour {
+
+	public Transform target;
+	Camera myCamera;
+
+
+	void Start () {
+		
+		myCamera = GetComponent<Camera> ();
+
+	}
+	
+
+	void FixedUpdate () {
+
+		//Auto_Resize
+		myCamera.orthographicSize = (Screen.height / 100f) / 4f;
+
+		//Smooth Follow
+		if (target)
+			transform.position = Vector3.Lerp (transform.position, target.position, 0.1f) + new Vector3 (0, 0, -10f);
+	}
+}
